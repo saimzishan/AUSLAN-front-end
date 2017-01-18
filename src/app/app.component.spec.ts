2899b951a193a68b7f4d8e9e569e06aea11d5444
NomiJ <nauman.qazi@systematicbytes.com>
2017-01-18 19:01:39 +1100

    chnaged incorrect test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,7 +2,7 @@
 
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
-
+import {GLOBAL} from './shared/global'
 describe('AppComponent', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -19,10 +19,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   }));
 
-  it(`should have as title 'app works!'`, async(() => {
+  it(`should have as title as declared in GLOBAL`, async(() => {
     let fixture = TestBed.createComponent(AppComponent);
     let app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app works!');
+    expect(app.title).toEqual(GLOBAL.TITLE + GLOBAL.VERSION);
   }));
 
   it('should render title in a h1 tag', async(() => {
