0da29497bfd134ca25b84cf0e5b358499b8116d0
NomiJ <nauman.qazi@systematicbytes.com>
2017-01-25 21:58:41 +1100

    CircleCI PACT Conf

--- karma.conf.js
+++ karma.conf.js
@@ -1,63 +1,65 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/0.13/config/configuration-file.html
 
-module.exports = function (config) {
-  config.set({
-    basePath: '',
-    frameworks: ['jasmine', 'angular-cli'],
-    plugins: [
-      require('karma-jasmine'),
-      require('karma-phantomjs-launcher'),
-      require('karma-remap-istanbul'),
-      require('angular-cli/plugins/karma'),
-      require('pact-consumer-js-dsl'),
-      require('karma-chrome-launcher'),
-      require('karma-safari-launcher'),
-      require('karma-safaritechpreview-launcher'),
-      require('karma-firefox-launcher')
+module.exports = function(config) {
+    config.set({
+        basePath: '',
+        frameworks: ['jasmine', 'angular-cli'],
+        plugins: [
+            require('karma-jasmine'),
+            require('karma-phantomjs-launcher'),
+            require('karma-remap-istanbul'),
+            require('angular-cli/plugins/karma'),
+            require('pact-consumer-js-dsl'),
+            require('karma-chrome-launcher'),
+            require('karma-safari-launcher'),
+            require('karma-safaritechpreview-launcher'),
+            require('karma-firefox-launcher')
 
-    ],
-    files: [
-	     'node_modules/pact-consumer-js-dsl/dist/pact-consumer-js-dsl.js',
-      { pattern: './src/test.ts', watched: false }
-    ],
-    preprocessors: {
-      './src/test.ts': ['angular-cli']
-    },
-    mime: {
-      'text/x-typescript': ['ts','tsx']
-    },
-    remapIstanbulReporter: {
-      reports: {
-        html: 'coverage',
-        lcovonly: './coverage/coverage.lcov'
-      }
-    },
-    angularCli: {
-      config: './angular-cli.json',
-      environment: 'dev'
-    },
-    reporters: config.angularCli && config.angularCli.codeCoverage
-              ? ['progress', 'karma-remap-istanbul']
-              : ['progress'],
+        ],
+        files: [
+            'node_modules/pact-consumer-js-dsl/dist/pact-consumer-js-dsl.js',
+            {
+                pattern: './src/test.ts',
+                watched: false
+            }
+        ],
+        preprocessors: {
+            './src/test.ts': ['angular-cli']
+        },
+        mime: {
+            'text/x-typescript': ['ts', 'tsx']
+        },
+        remapIstanbulReporter: {
+            reports: {
+                html: 'coverage',
+                lcovonly: './coverage/coverage.lcov'
+            }
+        },
+        angularCli: {
+            config: './angular-cli.json',
+            environment: 'dev'
+        },
+        reporters: config.angularCli && config.angularCli.codeCoverage ?
+            ['progress', 'karma-remap-istanbul'] :
+            ['progress'],
 
-    port: 9876,
-    colors: true,
-    logLevel: config.LOG_INFO,
-    autoWatch: true,
-browsers: [/*'PhantomJS_without_security','Chrome', */'Chrome_without_security'], //Removing Chrome because of CI error
-   customLaunchers: {
-      PhantomJS_without_security: {
-        base: 'PhantomJS',
-        flags: ['--web-security=false']
-      },
-      Chrome_without_security: {
-        base: 'Chrome',
-        flags: ['--disable-web-security']
-      }
-   }
-    ,
+        port: 9876,
+        colors: true,
+        logLevel: config.LOG_INFO,
+        autoWatch: true,
+        browsers: ['PhantomJS_without_security', /*'Chrome_without_security'*/ ], //Removing Chrome because of CI error
+        customLaunchers: {
+            PhantomJS_without_security: {
+                base: 'PhantomJS',
+                flags: ['--web-security=false']
+            },
+            Chrome_without_security: {
+                base: 'Chrome',
+                flags: ['--disable-web-security']
+            }
+        },
 
-    singleRun: false
-  });
+        singleRun: false
+    });
 };
