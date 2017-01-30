238c1f207a95ec5c30f116c8de27386fce8d678b
NomiJ <nauman.qazi@systematicbytes.com>
2017-01-30 19:52:24 +1100

    fixed newer pact path for user api running in conjuction with booking api

--- src/app/api/user.service.spec.ts
+++ src/app/api/user.service.spec.ts
@@ -2,7 +2,7 @@
 
 import { TestBed, async, inject } from '@angular/core/testing';
 import { UserService } from './user.service';
-
+import {GLOBAL} from '../shared/global';
 declare function require(name: string);
 let Pact = require('pact-consumer-js-dsl');
 
@@ -13,20 +13,29 @@ describe('UserService', () => {
       TestBed.configureTestingModule({
           providers: [UserService]
       });
-      helloProvider = Pact.mockService({
-            consumer: 'Booking-System-frontend',
-            provider: 'Booking-System-Api',
-            port: 1234,
-            done: done
-        });
+
         done();
     });
+    beforeAll(function(done) {
+      helloProvider = Pact.mockService({
+            consumer: 'Booking-System-frontend',
+            provider: 'User-Api',
+            port: GLOBAL.MOCK_SERVER_PORT,
+            done: done
+        });
+
+    // This ensures your pact-mock-service is in a clean state before
+    // running your test suite.
+    // helloProvider.resetSession(done);
+    done();
+  });
+
 
     afterAll(function(done) {
         helloProvider.finalize()
             .then(function() { done(); }, function(err) { done.fail(err); });
     });
-    it('should ...', function(done) {
+    it('should exists', function(done) {
       inject([UserService], (service: UserService) => {
         expect(service).toBeTruthy();
         done();
@@ -35,16 +44,19 @@ describe('UserService', () => {
     it('should say hello', function(done) {
         inject([UserService], (service: UserService) => {
           helloProvider
+              .given('user api will say hello')
               .uponReceiving('a request for hello')
-              .withRequest('get', '/sayHello')
+              .withRequest('GET', '/api/v1/users/sayHello', {
+                'Accept': '*/*'
+              })
               .willRespondWith(200, {
-                  'Content-Type': 'application/json'
+                  'Content-Type': 'application/json; charset=utf-8'
               }, {
                   reply: 'Hello'
               });
 
           helloProvider.run(done, function(runComplete) {
-              service.sayHello('http://127.0.0.1:1234')
+              service.sayHello(GLOBAL.USER_API)
                   .then(function(xhr) {
                       val = JSON.parse(xhr.responseText).reply;
                       expect(val).toEqual('Hello');
--- src/app/shared/global.ts
+++ src/app/shared/global.ts
@@ -2,8 +2,14 @@ import { environment } from '../../environments/environment';
 
 
 export class GLOBAL {
-  public static API_ENDPOINT = (environment.production) ? 'http://<prod_ip>:8080/api/v1' : 'http://localhost:8080/api/v1' ;
-  public static USER_API = GLOBAL.API_ENDPOINT + '/users/' ;
+  public static MOCK_SERVER_PORT = 1234;
+  public static LOG_LEVEL = 'INFO';
+  public static API_ENDPOINT = (environment.production) ? 'http://<prod_ip>:8080/api/v1' :
+  `http://127.0.0.1:${GLOBAL.MOCK_SERVER_PORT}/api/v1` ;
+  public static USER_API = GLOBAL.API_ENDPOINT + '/users' ;
+  public static USER_API_DEPRECIATED = 'http://localhost:8080/api/v1/users/' ;
+
+  public static BOOKING_API = GLOBAL.API_ENDPOINT + '/bookings' ;
   public static TITLE = 'Auslan ApBooking System' ;
   public static VERSION = '0.0.1' ; // This should be broken into MAJOR and MINOR version?
 
