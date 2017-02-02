1d207bbd44227788ddc953b38eb540149bc73f7b
Nauman Qazi <nauman.qazi@systematicbytes.com>
2017-02-02 22:36:46 +1100

    Added Pipe for Enum

---
+++ src/app/shared/pipe/enum-val.pipe.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { EnumValPipe } from './enum-val.pipe';
+
+describe('EnumValPipe', () => {
+  it('create an instance', () => {
+    let pipe = new EnumValPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
