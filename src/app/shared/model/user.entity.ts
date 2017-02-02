cce1a1e873856f68ca89af9f657f82bbd32f430b
Nauman Qazi <nauman.qazi@systematicbytes.com>
2017-02-02 22:36:21 +1100

    Fixed JSON Conversion to Ruby Format in Stringify

--- src/app/shared/model/user.entity.ts
+++ src/app/shared/model/user.entity.ts
@@ -2,35 +2,17 @@ import {ROLE} from './role.enum';
 
 export class User {
 
-  private _email: string;
-  private _name: string;
-  private _pass: string;
-  private _confirm_pass: string;
-  private _role: ROLE;
-  public _id: number;
+  public email = 'john@doe.com';
+  public password: string;
+  public confirm_password: string;
+  public role: ROLE;
+  public first_name = 'Major';
+  public last_name = 'Lazer';
+  public id: number;
 
-    constructor(values: Object = {}) {
-      Object.assign(this, values);
-    }
-
-    get id() { return this._id; }
-    get email() { return this._email; }
-    get name() { return this._name; }
-    get pass() { return this._pass; }
-    get role() { return this._role; }
-
-    set email(val: string) {
-        this._email = val;
-    }
-    set name(val: string) {
-        this._name = val;
-    }
-    set pass(val: string) {
-        this._pass = val;
-    }
-    set role(val: ROLE) {
-        this._role = val;
-    }
+  constructor(values: Object = {}) {
+    Object.assign(this, values);
+  }
 }
 
 export class OrganisationalRepresentative extends User {
