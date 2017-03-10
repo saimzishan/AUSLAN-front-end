043dea31db9e0db3d3f2c5dbd788b90dd5fd9467
NomiJ <nauman.qazi@systematicbytes.com>
2017-03-10 13:55:17 +1100

    booking-management fixed compilation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AuthComponent } from './auth/auth.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { UserManagementComponent } from './user-management/user-management.component';
 import { UserService } from './api/user.service';
+import { BookingService } from './api/booking.service';
 import { Http, RequestOptions } from '@angular/http';
 import { AuthHttp, AuthConfig } from 'angular2-jwt';
 import { EnumValPipe } from './shared/pipe/enum-val.pipe';
@@ -22,19 +23,19 @@ import { ResetComponent } from './reset/reset.component';
 import {AuthGuard} from './auth/auth.guard';
 import { NotificationComponent } from './notification/notification.component';
 import { BookingComponent } from './booking/booking.component';
-import { BookingDetailComponent } from './booking/booking-detail/booking-detail.component';
+import { BookingDetailComponent } from './booking-management/booking-detail/booking-detail.component';
 import { SpinnerComponent } from './spinner/spinner.component';
 import { SpinnerService } from './spinner/spinner.service';
 import { HeaderComponent } from './ui/header/header.component';
-import { FilterComponent } from './booking/filter/filter.component';
-import { BookingListComponent } from './booking/booking-list/booking-list.component';
+import { BookingFilterComponent } from './booking-management/booking-filter/booking-filter.component';
+import { BookingListComponent } from './booking-management/booking-list/booking-list.component';
 import { UserListComponent } from './user-management/user-list/user-list.component';
 import { UserFilterComponent } from './user-management/user-filter/user-filter.component';
 import { UserHeaderComponent } from './user-management/user-header/user-header.component';
 import { UserDetailComponent } from './user-management/user-detail/user-detail.component';
+import { BookingHeaderComponent } from './booking-management/booking-header/booking-header.component';
 import { MaterialModule } from '@angular/material';
 import {} from 'foundation-sites';
-import { BookingHeaderComponent } from './booking-management/booking-header/booking-header.component';
 
 
 @NgModule({
@@ -42,31 +43,31 @@ import { BookingHeaderComponent } from './booking-management/booking-header/book
     AppComponent,
     AuthComponent,
     DashboardComponent,
-    UserManagementComponent,
     EnumValPipe,
     NotFoundComponent,
     RegisterComponent,
     VerifyComponent,
     ResetComponent,
     NotificationComponent,
-    BookingComponent,
-    BookingDetailComponent,
     SpinnerComponent,
     HeaderComponent,
-    FilterComponent,
+    BookingComponent,
+    BookingDetailComponent,
     BookingListComponent,
-    UserListComponent,
+    BookingFilterComponent,
+    BookingHeaderComponent,
+    UserManagementComponent,
     UserFilterComponent,
+    UserListComponent,
     UserHeaderComponent,
     UserDetailComponent,
-    BookingHeaderComponent,
   ], exports: [EnumValPipe],
-   entryComponents: [UserDetailComponent],
+   entryComponents: [UserDetailComponent, BookingDetailComponent],
   imports: [CustomFormsModule, routing,
     BrowserModule, RouterModule,
     FormsModule, MaterialModule.forRoot(),
     HttpModule
-  ],  providers: [AuthGuard, Title, SpinnerService, UserService, {provide: APP_BASE_HREF, useValue : '/' }, {
+  ],  providers: [AuthGuard, Title, SpinnerService, BookingService, UserService, {provide: APP_BASE_HREF, useValue : '/' }, {
       provide: AuthHttp,
       useFactory: authService,
       deps: [Http, RequestOptions]
