a988ef8dfe03b416abce52b216e571fc23603eab
NomiJ <nauman.qazi@systematicbytes.com>
2017-03-15 18:13:00 +1100

    fixed lint + added bookign method

--- src/app/shared/model/booking.entity.ts
+++ src/app/shared/model/booking.entity.ts
@@ -5,6 +5,7 @@ import {BOOKING_STATUS} from './booking-status.enum';
 
 export class Booking {
     public id: string;
+    public number_of_people_attending: number;
     public venue: Venue = new Venue();
     public requested_by: Contact = new Contact();
     public last_updated: Date;
@@ -18,7 +19,19 @@ export class Booking {
     public attachment: any;
     public raw_venue_address: string;
     public raw_booking_address: string;
-    fromJSON(data: any){
+    fromJSON(data: any) {
 
+    }
+
+    toJSON() {
+      return `{"venue":${this.venue.addressline_1},"requested_by":${this.requested_by.name},
+      "nature_of_appointment":${this.nature_of_appointment},
+      "specific_nature_of_appointment":${this.specific_nature_of_appointment},"contact_name":${this.contact.name},
+      "contact_phone_number":${this.contact.phone_number},"contact_mobile_number":${this.contact.mobile_number},
+      "deaf_persons_name":${this.deaf_person.name},"deaf_persons_mobile":${this.deaf_person.name},
+      "deaf_persons_email":${this.deaf_person.email},"deaf_persons_eaf_no":${this.deaf_person.eaf},
+      "number_of_people_attending":${this.number_of_people_attending},
+      "start_time":${this.venue.start_time},"end_time":${this.venue.end_time},
+      "parking_availability":${this.venue.parking_type}}`;
     }
 }
