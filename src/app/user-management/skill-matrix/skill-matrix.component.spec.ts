e28698869375a61a64a4e88c5e1909ef4a30b73f
NomiJ <nauman.qazi@systematicbytes.com>
2017-05-31 13:43:36 +1000

    fixed test

--- src/app/user-management/skill-matrix/skill-matrix.component.spec.ts
+++ src/app/user-management/skill-matrix/skill-matrix.component.spec.ts
@@ -1,6 +1,9 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SkillMatrixComponent } from './skill-matrix.component';
+import {EnumValPipe} from '../../shared/pipe/enum-val.pipe';
+import {CustomFormsModule} from 'ng2-validation';
+import {MaterialModule} from '@angular/material';
 
 describe('SkillMatrixComponent', () => {
   let component: SkillMatrixComponent;
@@ -8,7 +11,10 @@ describe('SkillMatrixComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ SkillMatrixComponent ]
+      declarations: [ SkillMatrixComponent, EnumValPipe ]
+      ,
+      imports: [CustomFormsModule, MaterialModule
+      ]
     })
     .compileComponents();
   }));
