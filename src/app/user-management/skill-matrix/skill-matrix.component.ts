aab9a9235354d28957eac0a60eb8d1c3fb30ca3f
NomiJ <nauman.qazi@systematicbytes.com>
2017-05-31 13:36:25 +1000

    basic skill matrix

---
+++ src/app/user-management/skill-matrix/skill-matrix.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import {BA, BOOKING_NATURE} from '../../shared/model/booking-nature.enum';
+
+@Component({
+  selector: 'app-skill-matrix',
+  templateUrl: './skill-matrix.component.html',
+  styleUrls: ['./skill-matrix.component.css']
+})
+export class SkillMatrixComponent {
+  raw_nature_of_appointment: string;
+  userDisplayName: string;
+  appointment_types = Object.keys(BOOKING_NATURE).filter(value => value === BOOKING_NATURE[value]
+  || BOOKING_NATURE[value].startsWith(value)).map(v => BOOKING_NATURE[v]) as string[];
+
+  specific_appointment_types =  [];
+  constructor() {
+    BA.loadItems();
+  }
+
+  natureOfApptChange($event) {
+    let val: BOOKING_NATURE = <BOOKING_NATURE> BOOKING_NATURE[this.raw_nature_of_appointment];
+    this.specific_appointment_types = BA.DISSCUSSION_ITEM[BOOKING_NATURE[val]];
+  }
+}
