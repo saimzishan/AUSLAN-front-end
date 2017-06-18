1e1e7c0676b535e5e2d20a1e3c4f763880553ee3
NomiJ <nauman.qazi@systematicbytes.com>
2017-06-19 00:58:50 +1000

    fixing skill matrix

--- src/app/user-management/skill-matrix/skill-matrix.component.ts
+++ src/app/user-management/skill-matrix/skill-matrix.component.ts
@@ -1,38 +1,108 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {BA, BOOKING_NATURE} from '../../shared/model/booking-nature.enum';
+import {ActivatedRoute} from '@angular/router';
+import {UserService} from '../../api/user.service';
+import {NotificationServiceBus} from '../../notification/notification.service';
+import {SpinnerService} from '../../spinner/spinner.service';
+import {Interpreter} from '../../shared/model/user.entity';
 
 @Component({
-  selector: 'app-skill-matrix',
-  templateUrl: './skill-matrix.component.html',
-  styleUrls: ['./skill-matrix.component.css']
+    selector: 'app-skill-matrix',
+    templateUrl: './skill-matrix.component.html',
+    styleUrls: ['./skill-matrix.component.css']
 })
-export class SkillMatrixComponent {
-  raw_nature_of_appointment: string;
-  userDisplayName: string;
-  appointment_types = Object.keys(BOOKING_NATURE).filter(value => value === BOOKING_NATURE[value]
-  || BOOKING_NATURE[value].startsWith(value)).map(v => BOOKING_NATURE[v]) as string[];
-  currentLevel= '';
-  levels= ['1', '2', '3', 'n/a'];
-  specific_appointment_types =  [];
-  doSave = false;
-  constructor() {
-    BA.loadItems();
-    this.natureOfApptChange('Human Services');
-  }
+export class SkillMatrixComponent implements OnInit {
+    raw_nature_of_appointment: string;
+    userDisplayName: string;
+    appointment_types = Object.keys(BOOKING_NATURE).filter(value => value === BOOKING_NATURE[value]
+    || BOOKING_NATURE[value].startsWith(value)).map(v => BOOKING_NATURE[v]) as string[];
+    currentLevel = '';
+    levels = ['1', '2', '3', 'n/a'];
+    specific_appointment_types = [];
+    doSave = false;
+    sub;
+    userModel: Interpreter;
 
-  skillChanged(event) {
-    this.doSave =  event.checked;
-  }
+    constructor(public spinnerService: SpinnerService,
+                public notificationServiceBus: NotificationServiceBus,
+                private route: ActivatedRoute, private userService: UserService) {
+        BA.loadItems();
+    }
 
-  natureOfApptChange(value) {
-    this.raw_nature_of_appointment = value;
-    let val: BOOKING_NATURE = <BOOKING_NATURE> BOOKING_NATURE[this.raw_nature_of_appointment];
-    this.specific_appointment_types = BA.DISSCUSSION_ITEM[BOOKING_NATURE[val]];
-    this.currentLevel = '';
-    this.doSave = false;
-  }
+    ngOnInit() {
+        this.sub = this.route.params.subscribe(params => {
+            let param_id = params['id'] || '';
+            if (Boolean(param_id) && parseInt(param_id, 10) > 0) {
+                this.getInterpreterDetail(param_id);
+            }
+        });
+    }
 
-  apply(val: boolean) {
+    getInterpreterDetail(param_id) {
+        this.spinnerService.requestInProcess(true);
+        this.userService.getUser(param_id)
+            .subscribe((res: any) => {
+                    if (res.status === 200) {
+                        this.userModel = res.data;
+                        this.natureOfApptChange('Human Services');
 
-  }
+                    }
+                    this.spinnerService.requestInProcess(false);
+                },
+                err => {
+                    this.spinnerService.requestInProcess(false);
+                    let e = err.json() || 'There is some error on server side';
+                    this.notificationServiceBus.launchNotification(true, err.statusText + ' ' + e.errors);
+                });
+    }
+
+
+    skillChanged(event, level: string, raw_specific_booking_type: string) {
+
+        let assignment = this.userModel.assignments_attributes.filter(a => a.assignment_type_category_name
+        === this.raw_nature_of_appointment
+        && a.assignment_type_name === raw_specific_booking_type);
+        if (Boolean(assignment) && assignment.length > 0) {
+            let prev = assignment[0].level;
+            assignment[0].level = level === 'n/a' ? 0 : parseInt(level, 10);
+            this.doSave = assignment[0].level !== prev;
+
+        }
+    }
+
+    isChecked(level: string, raw_specific_booking_type: string) {
+        if (Boolean(this.userModel.assignments_attributes)) {
+
+            let assignment = this.userModel.assignments_attributes.filter(a => a.assignment_type_category_name
+            === this.raw_nature_of_appointment
+            && a.assignment_type_name === raw_specific_booking_type);
+            return (Boolean(assignment) && assignment.length > 0 ) && (assignment[0].level.toString() === level
+                || (level === 'n/a' && assignment[0].level === 0));
+        }
+        return false;
+    }
+
+    natureOfApptChange(value) {
+
+        this.raw_nature_of_appointment = value;
+        let val: BOOKING_NATURE = <BOOKING_NATURE> BOOKING_NATURE[this.raw_nature_of_appointment];
+        this.specific_appointment_types = BA.DISSCUSSION_ITEM[BOOKING_NATURE[val]];
+        this.currentLevel = '';
+        this.doSave = false;
+    }
+
+    apply(val: boolean) {
+        this.userService.updateUser(this.userModel)
+            .subscribe((res: any) => {
+                    if (res.status === 204) {
+                        // UI Notification
+                        this.notificationServiceBus.launchNotification(false, 'User details updated Successfully');
+                    }
+                },
+                (err) => {
+                    this.spinnerService.requestInProcess(false);
+                    this.notificationServiceBus.launchNotification(true, err);
+                });
+
+    }
 }
