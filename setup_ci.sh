e338cd82257a94cef7230172dcbe50cd129b5ef6
mohjay <mohinder@curvetomorrow.com.au>
2017-08-29 23:31:21 +0800

    adding back in old code

--- setup_ci.sh
+++ setup_ci.sh
@@ -5,7 +5,8 @@ git clone git@bitbucket.org:curvetomorrow/booking-system-api.git ../booking-syst
 cd ../booking-system-api 
 bundle install
 service postgresql restart
-nohup rails s > rails_server.log 2>ouput & sleep 1
+nohup rails s > rails_server.log 2>&1 &
+sleep 10
 ps -ef | grep "puma"
 cd ../booking-system-frontend
 npm install
