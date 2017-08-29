89fee8b25c72b8ebb42e7dcbd135822f31c4e9a1
mohjay <mohinder@curvetomorrow.com.au>
2017-08-29 14:57:50 +0800

    took out debug lines

--- setup_ci.sh
+++ setup_ci.sh
@@ -5,7 +5,6 @@ git clone git@bitbucket.org:curvetomorrow/booking-system-api.git ../booking-syst
 cd ../booking-system-api 
 bundle install
 service postgresql restart
-bundle exec rake -T
 nohup rails s > rails_server.log 2>&1 &
 sleep 10
 ps -ef | grep "puma"
