38270446379f38edf262455932e54ae5b63b3d3d
mohjay <mohinder@curvetomorrow.com.au>
2017-08-29 22:43:39 +0800

    try again

--- setup_ci.sh
+++ setup_ci.sh
@@ -5,10 +5,12 @@ git clone git@bitbucket.org:curvetomorrow/booking-system-api.git ../booking-syst
 cd ../booking-system-api 
 bundle install
 service postgresql restart
-nohup rails s > rails_server.log 2>&1 &
-sleep 10
+nohup rails s > rails_server.log 2>ouput & sleep 1
 ps -ef | grep "puma"
 cd ../booking-system-frontend
-npm install
-sh run-test.sh
+pwd
+cd ..
+ls -la
+# npm install
+# sh run-test.sh
 
