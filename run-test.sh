e41f119b5c0beccea0425171652621c7a0fc3cb3
mohjay <mohinder@curvetomorrow.com.au>
2017-08-29 22:11:16 +0800

    trying split again

--- run-test.sh
+++ run-test.sh
@@ -1,4 +1,4 @@
-for file in e2e-tests/features/*.feature
+for file in $(cat test_features_for_split_container.txt)
 do    
 if grep -q mobile "$file"; then
     ng e2e --env=localhost  --progress=true --specs=$file --conf protractor.conf.mobile.js
