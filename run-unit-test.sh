8ce9d2d23571a911750d1cdb6f67fba4aa7c88ca
mohjay <mohinder@curvetomorrow.com.au>
2017-08-30 12:30:49 +0800

    edited wrong file

--- run-unit-test.sh
+++ run-unit-test.sh
@@ -1,9 +1,11 @@
-for file in $(cat test_features_for_split_container.txt)
-do    
-if grep -q mobile "$file"; then
-    ng e2e --env=localhost  --progress=true --specs=$file --conf protractor.conf.mobile.js
- else
-   ng e2e --env=localhost  --progress=true --specs=$file
-fi 
-done
+ng lint
+rm -rf tmp
+bundle exec pact-mock-service start -p 1233 -l tmp/booking/pact-booking.log --pact-dir tmp/booking/pacts
+bundle exec pact-mock-service start -p 1234 -l tmp/user/pact-user.log --pact-dir tmp/user/pacts
+nohup npm run startwebdriver > webdriver.log 2>&1 &
+sleep 10
+npm run circle-test
+npm run pact
+bundle exec pact-mock-service stop -p 1233
+bundle exec pact-mock-service stop -p 1234
 
