039b20f566d64701cea16b082a7f651e0833ddf2
mohjay <mohinder@curvetomorrow.com.au>
2017-08-30 13:48:38 +0800

    removed redundant test setup

--- run-unit-test.sh
+++ run-unit-test.sh
@@ -3,8 +3,6 @@ ng lint
 rm -rf tmp
 bundle exec pact-mock-service start -p 1233 -l tmp/booking/pact-booking.log --pact-dir tmp/booking/pacts
 bundle exec pact-mock-service start -p 1234 -l tmp/user/pact-user.log --pact-dir tmp/user/pacts
-nohup npm run startwebdriver > webdriver.log 2>&1 &
-sleep 10
 npm run circle-test
 npm run pact
 bundle exec pact-mock-service stop -p 1233
