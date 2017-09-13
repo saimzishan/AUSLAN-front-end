4d6678bfc56b3fd34dffc0eab8a067e713cf0760
Manu Dwivedi <manu@curvetomorrow.com.au>
2017-09-13 13:39:03 +0530

    Add interface for booking filters

---
+++ src/app/shared/model/booking-filter.interface.ts
@@ -0,0 +1,12 @@
+export interface BookingFilter {
+    booking_ids?: string;
+    state?: string;
+    booking_status?: string;
+    client_name?: string;
+    interpreter_ids?: string;
+    booking_type?: string;
+    suburb?: string;
+    organisation?: string;
+    date_from?: string;
+    date_to?: string;
+}
