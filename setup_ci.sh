fa9082b4ea3e6136a70169eb033288f028849937
mohjay <mohinder@curvetomorrow.com.au>
2017-09-15 22:47:43 +0800

    start DB early

--- setup_ci.sh
+++ setup_ci.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 set -x #echo on
 export CHROME_BIN=$(which google-chrome)
+service postgresql restart
 gem install bundler
 git clone git@bitbucket.org:curvetomorrow/booking-system-api.git ../booking-system-api
 cd ../booking-system-api
 bundle install --path vendor/cache
 bundle exec rake db:migrate
 spring stop
-service postgresql restart
 nohup rails s -b 0.0.0.0 > rails_server.log 2>&1 &
 sleep 5
 ps -ef | grep "puma"
