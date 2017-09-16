b5d0d70eaecfd79b32975fb043bba68d8298d1fe
mohjay <mohinder@curvetomorrow.com.au>
2017-09-16 19:42:51 +0800

    trying latest with new build stuff

--- setup_ci.sh
+++ setup_ci.sh
@@ -2,13 +2,14 @@
 set -x #echo on
 set -eo pipefail
 export CHROME_BIN=$(which google-chrome)
+service postgresql restart
 gem install bundler
 #ssh-agent bash -c 'ssh-add ~/.ssh/id_rsa.pub; git clone git@bitbucket.org:curvetomorrow/booking-system-api.git ../booking-system-api'
 git clone  --verbose git@bitbucket.org:curvetomorrow/booking-system-api.git ../booking-system-api
 cd ../booking-system-api
 bundle install --path vendor/cache
-service postgresql restart
-nohup rails s -b 0.0.0.0 > rails_server.log 2>&1 &
+bundle exec rake db:migrate
+nohup bundle exec rails s -b 0.0.0.0 > rails_server.log 2>&1 &
 sleep 5
 ps -ef | grep "puma"
 cd ../booking-system-frontend
