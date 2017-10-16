76a95e2948933231360a6a8b8969631a2285f0d1
mohjay <mohinder@curvetomorrow.com.au>
2017-10-16 21:22:03 +0800

    Added UI tests for Interpreter name filter

--- e2e-tests/features/step_definitions/booking-management.steps.ts
+++ e2e-tests/features/step_definitions/booking-management.steps.ts
@@ -21,6 +21,8 @@ defineSupportCode(({Given, Then, When}) => {
     Given(/^There exist (\d+) bookings$/, Heroku.createBulkBookings);
     Given(/^One booking has client name as '(.*)'$/, Heroku.updateBookingWithClientName)
     Given(/^One booking has client last name as '(.*)'$/, Heroku.updateBookingWithLastClientName)
+    Given(/^One booking has interpreter first name as '(.*)'$/, Heroku.updateBookingWithInterpreterFirstName)
+    Given(/^One booking has interpreter last name as '(.*)'$/, Heroku.updateBookingWithInterpreterLastName)
     Given(/^One booking has org name as '(.*)'$/, Heroku.updateBookingWithOrgName)
     Given(/^One booking has suburb as '(.*)'$/, Heroku.updateBookingWithSuburb)
     Given(/^One booking has start and end dates as first and last days of next week$/, Heroku.updateBookingStartAndEndDateTime)
@@ -82,6 +84,7 @@ defineSupportCode(({Given, Then, When}) => {
     When(/^I click on option (.*) of (.*) for (.*)/, createBookingPO.clickOnOption);
     When(/^I query booking with booking id$/, bookingManagementPO.queryBookingWithID)
     When(/^I query booking with client name '(.*)'$/, bookingManagementPO.queryBookingByClientName)
+    When(/^I query booking with interpreter name '(.*)'$/, bookingManagementPO.queryBookingByInterpreterName)
     When(/^I query booking with org name '(.*)'$/, bookingManagementPO.queryBookingByOrgName);
     When(/^I query booking with suburb '(.*)'$/, bookingManagementPO.queryBookingBySuburb);
     When(/^I filter booking by date range first and last days of next week$/, bookingManagementPO.filterBookingByDateRange);
@@ -99,6 +102,8 @@ defineSupportCode(({Given, Then, When}) => {
     Then(/^I see one row with the booking id$/, bookingManagementPO.bookingExistsWithId);
     Then(/^I see one row with client name '(.*)'$/, bookingManagementPO.bookingExistsWithClientName);
     Then(/^I see one row with client last name '(.*)'$/, bookingManagementPO.bookingExistsWithClientLastName);
+    Then(/^I see one row with interpreter first name '(.*)'$/, bookingManagementPO.bookingExistsWithInterpreterFirstName);
+    Then(/^I see one row with interpreter last name '(.*)'$/, bookingManagementPO.bookingExistsWithInterpreterLastName);
     Then(/^I see one row with org name '(.*)'$/, bookingManagementPO.bookingExistsWithOrgName);
     Then(/^I see one row with suburb '(.*)'$/, bookingManagementPO.bookingExistsWithSuburb);
 
