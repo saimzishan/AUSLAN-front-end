e49a339b5ac0c7cc2271acefe0acce8e9d1e69c2
Ammad Ahmed <ammadahmed23@gmail.com>
2017-10-18 14:08:57 +0500

    Added logic to handle the required fields of address component

--- src/app/ui/address/address.component.ts
+++ src/app/ui/address/address.component.ts
@@ -1,12 +1,15 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ViewChild } from '@angular/core';
 import { Address } from '../../shared/model/venue.entity';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-address',
   templateUrl: './address.component.html',
-  styleUrls: ['./address.component.css']
+  styleUrls: ['./address.component.css'],
+  exportAs: 'ctAddressForm'
 })
 export class AddressComponent {
   @Input() address: Address;
   @Input() prefix = '';
+  @ViewChild('addressFields') public form: NgForm;
 }
