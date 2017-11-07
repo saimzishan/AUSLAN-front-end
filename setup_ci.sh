a09cbeb4bebba608d77a97bf2ea214d46fe88006
Manu Dwivedi <manu@curvetomorrow.com.au>
2017-11-07 21:15:37 +0530

    Change setup_ci to point to master

--- setup_ci.sh
+++ setup_ci.sh
@@ -4,9 +4,8 @@ set -eo pipefail
 export CHROME_BIN=$(which google-chrome)
 service postgresql restart
 gem install bundler
-git clone  --verbose git@bitbucket.org:curvetomorrow/booking-system-api.git ../booking-system-api
+git clone  --depth 1 --branch master --verbose git@bitbucket.org:curvetomorrow/booking-system-api.git ../booking-system-api
 cd ../booking-system-api
-git checkout 4b969fd7e0fe9906a3d9b522e02e2ee790315ccc
 bundle install --path vendor/cache
 bundle exec rake db:migrate
 nohup bundle exec rails s -b 0.0.0.0 > rails_server.log 2>&1 &
