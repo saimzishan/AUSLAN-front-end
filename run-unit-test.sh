83da45f35f10171bdf43c291c3ffde502c637967
NomiJ <nauman.qazi@systematicbytes.com>
2017-11-09 23:03:18 +1100

    enable test

--- run-unit-test.sh
+++ run-unit-test.sh
@@ -6,6 +6,7 @@ ng lint
 rm -rf tmp
 bundle exec pact-mock-service start -p 1233 -l tmp/booking/pact-booking.log --pact-dir tmp/booking/pacts
 bundle exec pact-mock-service start -p 1234 -l tmp/user/pact-user.log --pact-dir tmp/user/pacts
+npm run circle-test
 npm run pact
 bundle exec pact-mock-service stop -p 1233
 bundle exec pact-mock-service stop -p 1234
