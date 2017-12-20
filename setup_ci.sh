77a09b33fc5db9fa4fe106c0df64094eb02a3996
Nitin Srivastava <nitin@curvetomorrow.com.au>
2017-12-20 19:12:20 +0530

    Remove test env

--- setup_ci.sh
+++ setup_ci.sh
@@ -4,7 +4,6 @@ set -eo pipefail
 apt-get update
 apt-get --only-upgrade install google-chrome-stable
 export CHROME_BIN=$(which google-chrome)
-export RAILS_ENV=test 
 service postgresql restart
 gem install bundler
 git clone  --depth 1 --branch master --verbose git@bitbucket.org:curvetomorrow/booking-system-api.git ../booking-system-api
@@ -13,7 +12,7 @@ bundle install --path vendor/cache
 bundle exec rake db:drop
 bundle exec rake db:create
 bundle exec rake db:migrate
-nohup bundle exec rails s -e test -b 0.0.0.0 > rails_server.log 2>&1 &
+nohup bundle exec rails s -b 0.0.0.0 > rails_server.log 2>&1 &
 sleep 5
 ps -ef | grep "puma"
 cd ../booking-system-frontend
