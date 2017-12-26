07a0555842ca54b9628ba4fa39b8d340e71cccc8
Manu Dwivedi <manu@curvetomorrow.com.au>
2017-12-26 19:11:00 +0530

    Only admin and BO should see link id. Includes: Code reformatting

--- src/app/booking-management/booking-list/booking-list.component.ts
+++ src/app/booking-management/booking-list/booking-list.component.ts
@@ -1,14 +1,19 @@
-import {Component, Input, Output, OnInit, EventEmitter, SimpleChanges} from '@angular/core';
+import {Component, Input, Output, OnInit, EventEmitter} from '@angular/core';
 import {URLSearchParams} from '@angular/http';
 import {Booking} from '../../shared/model/booking.entity';
-import {Router, NavigationExtras} from '@angular/router';
+import {Router} from '@angular/router';
 import {BOOKING_STATE} from '../../shared/model/booking-state.enum';
 import {BOOKING_STATUS} from '../../shared/model/booking-status.enum';
 import {GLOBAL} from '../../shared/global';
-import {IndividualClient, Interpreter, OrganisationalRepresentative} from '../../shared/model/user.entity';
+import {
+    Administrator,
+    BookingOfficer,
+    IndividualClient,
+    Interpreter,
+    OrganisationalRepresentative
+} from '../../shared/model/user.entity';
 import {BookingInterpreter} from '../../shared/model/contact.entity';
 import {BookingFilter} from '../../shared/model/booking-filter.interface';
-import {FormGroup, NgForm} from '@angular/forms';
 import {BA, BOOKING_NATURE} from '../../shared/model/booking-nature.enum';
 
 @Component({
@@ -21,20 +26,16 @@ export class BookingListComponent implements OnInit {
     @Output() onBookingFilter = new EventEmitter();
     bookingFilter: BookingFilter = {};
     private filterParams = new URLSearchParams();
-    private currentSort = {'field': 'job', 'order': 'asc' };
+    private currentSort = {'field': 'job', 'order': 'asc'};
     @Output() onPageEmit = new EventEmitter<number>();
     @Input() p = 1;
     @Input() totalItems = 0;
-    private validKeys(list): Array<string> {
-        let keys = Object.keys(list);
-        return keys.slice(keys.length / 2);
-    };
 
     constructor(public router: Router) {
         BA.loadItems();
     }
 
-    ngOnInit () {
+    ngOnInit() {
         this.filterParams = GLOBAL._filterVal;
         this.filterParams.paramsMap.forEach((value: string[], key: string) => {
             for (let v of value) {
@@ -44,8 +45,11 @@ export class BookingListComponent implements OnInit {
             }
         });
     }
+
     underScoreToSpaces(str: string) {
-        if (!str) { return 'All'; }
+        if (!str) {
+            return 'All';
+        }
         return str.replace(/_/g, ' ');
     }
 
@@ -79,39 +83,51 @@ export class BookingListComponent implements OnInit {
         return interpreters.filter(i => i.id === GLOBAL.currentUser.id).length > 0;
     }
 
+    isCurrentUserAdminOrBookingOfficer(): boolean {
+        return Boolean(GLOBAL.currentUser instanceof Administrator ||
+            GLOBAL.currentUser instanceof BookingOfficer);
+    }
+
     didInterpreterAccepted(interpreters: Array<BookingInterpreter>) {
         return interpreters.filter(i => i.state === 'Accepted').slice(0, 3);
     }
+
     statusList() {
         let keys = Object.keys(BOOKING_STATUS);
         return ['All', ...keys.slice(keys.length / 2)];
 
     }
+
     stateList() {
         let keys = Object.keys(BOOKING_STATE);
         keys = keys.slice(keys.length / 2);
         keys.splice(0, 1);
         return ['All', ...keys];
     }
+
     assignmentCategoryList() {
         let keys = Object.keys(BA.DISSCUSSION_ITEM) as Array<string>;
         return ['All', ...keys];
     }
+
     filterStatus() {
         return BOOKING_STATUS[this.bookingFilter.booking_status];
     }
+
     private formatterValueFor(field: string, value: string) {
         let formattedValue: string;
-        if (value.toLowerCase() === 'all' ) { return ''; };
+        if (value.toLowerCase() === 'all') {
+            return '';
+        }
         if (value && value.length) {
             value = value.trim();
             value = value.replace(/,$/g, '');
             switch (field) {
                 case 'booking_status':
-                    formattedValue = BOOKING_STATUS.hasOwnProperty(value) ? BOOKING_STATUS[value].toString() : '' ;
+                    formattedValue = BOOKING_STATUS.hasOwnProperty(value) ? BOOKING_STATUS[value].toString() : '';
                     break;
                 case 'booking_type':
-                    formattedValue = BOOKING_NATURE.hasOwnProperty(value) ? BOOKING_NATURE[value].toString() : '' ;
+                    formattedValue = BOOKING_NATURE.hasOwnProperty(value) ? BOOKING_NATURE[value].toString() : '';
                     break;
                 default:
                     formattedValue = value;
@@ -120,6 +136,7 @@ export class BookingListComponent implements OnInit {
         }
         return formattedValue;
     }
+
     filter(field: string, value: string) {
         this.bookingFilter[field] = this.formatterValueFor(field, value);
         for (let k in this.bookingFilter) {
@@ -130,9 +147,11 @@ export class BookingListComponent implements OnInit {
         GLOBAL._filterVal = this.filterParams;
         this.onBookingFilter.emit();
     }
+
     private isCurrentSort(field: string) {
         return this.currentSort.field === field;
     }
+
     private setCurrentSort(field: string) {
         let order = 'asc';
         if (this.isCurrentSort(field)) {
@@ -141,9 +160,11 @@ export class BookingListComponent implements OnInit {
         this.currentSort.field = field;
         this.currentSort.order = order;
     }
+
     getSortOrder(field: string) {
         return this.isCurrentSort(field) ? this.currentSort.order : '';
     }
+
     sort(field: string) {
         this.setCurrentSort(field);
         this.filterParams.set('sort', this.currentSort.field);
@@ -151,6 +172,7 @@ export class BookingListComponent implements OnInit {
         GLOBAL._filterVal = this.filterParams;
         this.onBookingFilter.emit();
     }
+
     getPage(page: number) {
         this.onPageEmit.emit(page);
     }
