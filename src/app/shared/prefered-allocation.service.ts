e0d0bd64288b239253f5326062af081e517f3931
NomiJ <nauman.qazi@systematicbytes.com>
2018-01-05 10:44:04 +1100

    skeleton changes for preffered interpreter

--- src/app/shared/prefered-allocation.service.ts
+++ src/app/shared/prefered-allocation.service.ts
@@ -1,5 +1,5 @@
-import { Injectable } from '@angular/core';
-import { Subject }    from 'rxjs/Subject';
+import {Injectable} from '@angular/core';
+import {Subject} from 'rxjs/Subject';
 
 
 @Injectable()
@@ -15,4 +15,32 @@ export class PreferedAllocationService {
     publishData(data: Array<any>) {
         this.interpreterList.next(data);
     }
+
+    handlePreference(backupPrefferedAllocations, currentInterpreterID, isEditBooking, action) {
+        /*
+        * Check whether the interpreter is already in the list. If yes, then apply action (delete, undelete, change preference)
+        */
+        let retPrefferedAllocations = backupPrefferedAllocations;
+        let interpreterExists = backupPrefferedAllocations.filter(i => i.interperter_id === currentInterpreterID).length > 0;
+        switch (action) {
+            case 'delete':
+                if (interpreterExists) {
+                    /* Remove from API */
+                } else {
+                    /* Remove locally*/
+                }
+                break;
+            case 'add':
+                if (interpreterExists) {
+                    /* Add in API */
+                } else {
+                    /* Add in locally*/
+                }
+                break;
+        }
+
+        return retPrefferedAllocations;
+
+    }
+
 }
