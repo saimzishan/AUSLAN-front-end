4b33564d1238b83de3385f0ccc6be65f53d3fc26
NomiJ <nauman.qazi@systematicbytes.com>
2018-01-06 13:26:15 +1100

    fixed 829

--- src/app/shared/prefered-allocation.service.ts
+++ src/app/shared/prefered-allocation.service.ts
@@ -15,36 +15,4 @@ export class PreferedAllocationService {
     publishData(data: Array<any>) {
         this.interpreterList.next(data);
     }
-
-    handlePreference(backupPrefferedAllocations, selectedInterpreter, isPreffered, action) {
-        /*
-        * Check whether the interpreter is already in the list. If yes, then apply action (delete, undelete, change preference)
-        */
-        let retPrefferedAllocations = [];
-        let interpreterExists = backupPrefferedAllocations.filter(i => i.interperter_id === selectedInterpreter.interpreter_id).length > 0;
-        switch (action) {
-            case 'delete':
-                if (interpreterExists) {
-                    /* Remove from API */
-                    selectedInterpreter._destroy = 1;
-                } else {
-                    /* Remove locally*/
-                    retPrefferedAllocations.filter(i => i.interperter_id === selectedInterpreter.interpreter_id);
-                }
-                break;
-            case 'add':
-                if (interpreterExists) {
-                    /* Add in API */
-                    selectedInterpreter._destroy = 0;
-                } else {
-                    /* Add in locally*/
-                    retPrefferedAllocations.push(selectedInterpreter);
-                }
-                break;
-        }
-
-        return retPrefferedAllocations;
-
-    }
-
 }
