e009910ab491fbc7108d468b384a809b8eca5738
mohjay <mohinder@curvetomorrow.com.au>
2018-01-18 16:14:13 +0800

    update Km check

--- e2e-tests/po/booking-job.po.ts
+++ e2e-tests/po/booking-job.po.ts
@@ -57,14 +57,14 @@ export class BookingJobPage extends PageObject {
                 cols.each((col, index) => {
                     if (index === 8) {
                         col.getText().then((txt) => {
-                            expect(txt.trim()).to.be.eq('-');
-                        })
+                            expect(txt.trim()).to.be.eq('1.1 Km');
+                        });
                     }
 
                     if (index === 9) {
                         col.getText().then((txt) => {
                             expect(txt.trim()).to.be.eq('No');
-                        })
+                        });
                     }
                 });
             });
