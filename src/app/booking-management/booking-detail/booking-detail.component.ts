aea7b7c63ac0cb4fed02aede1d0738dec6319a1a
osamaaay <osamaalic@gmail.com>
2018-02-20 10:32:09 +0500

    comment removed

--- src/app/booking-management/booking-detail/booking-detail.component.ts
+++ src/app/booking-management/booking-detail/booking-detail.component.ts
@@ -547,7 +547,6 @@ export class BookingDetailComponent implements OnInit, OnDestroy {
       Calling this method will create a new booking
     */
     public onCreateBooking(form: any, addressForm: any, billingForm: any, uploader: FileUploader) {
-        // this.form.controls.markAsTouched({ onlySelf: true });
         if (addressForm.isTravelCostApplicable && !form.value.travel_cost_applicable) {
             this.notificationServiceBus.launchNotification(true, 'Travel cost must be applicable as your booking distance is more than 40 kms');
             return;
