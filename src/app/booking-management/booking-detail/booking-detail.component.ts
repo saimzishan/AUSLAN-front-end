3b2e246c82da6feecb7e97fc3b227f0987a03674
ammad <ammadahmed23@gmail.com>
2018-02-24 14:12:58 +0500

    state confirmation for unlinked bookings and card 1101

--- src/app/booking-management/booking-detail/booking-detail.component.ts
+++ src/app/booking-management/booking-detail/booking-detail.component.ts
@@ -308,6 +308,7 @@ export class BookingDetailComponent implements OnInit, OnDestroy {
             if (!this.forEdit) {
                 this.onSelectionChange();
                 this.onClientSelectionChange();
+                this.onStandardInvoice();
             }
             this.getUser();
             this.bookingModel.bookable_type = this.bookingModel.bookable_type || 'IndividualClient';
