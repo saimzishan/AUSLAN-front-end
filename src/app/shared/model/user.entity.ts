a25f2c43c918331894a707a0d336d0f0c6e6cb82
mbilal25 <mbilal5445@gmail.com>
2018-03-13 18:14:41 +0500

    add approve date field in interpreter

--- src/app/shared/model/user.entity.ts
+++ src/app/shared/model/user.entity.ts
@@ -421,6 +421,7 @@ export class Interpreter extends User {
     public naati_validity_end_date: string;
     public business_hours_phone: string;
     public date_of_birth: string;
+    public date_approved: string;
     public address_attributes: Address = new Address();
     public long_term_availability: interpreter_avalability;
     public override_availabilty: blockout_availability;
