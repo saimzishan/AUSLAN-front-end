1a7ae81a01023f23dc95c311cbe46a1577beecd5
Manu Dwivedi <manu@curvetomorrow.com.au>
2018-03-24 19:37:03 +0530

    Add export_type to API request

--- src/app/reports/reports.component.ts
+++ src/app/reports/reports.component.ts
@@ -6,42 +6,46 @@ import {BookingService} from '../api/booking.service';
 import {DatePipe} from '@angular/common';
 
 @Component({
-  selector: 'app-reports',
-  templateUrl: './reports.component.html',
-  styleUrls: ['./reports.component.css']
+    selector: 'app-reports',
+    templateUrl: './reports.component.html',
+    styleUrls: ['./reports.component.css']
 })
 export class ReportsComponent {
-  date_from: Date;
-  date_to: Date;
-  exportType = '';
+    date_from: Date;
+    date_to: Date;
+    exportType = '';
 
-  constructor(public notificationServiceBus: NotificationServiceBus, public spinnerService: SpinnerService,
-              public bookingService: BookingService, private datePipe: DatePipe) { }
+    constructor(public notificationServiceBus: NotificationServiceBus, public spinnerService: SpinnerService,
+        public bookingService: BookingService, private datePipe: DatePipe) { }
 
-  exportClick(report_form) {
-    if (report_form.invalid) {
-      this.notificationServiceBus.launchNotification(true, 'Please select all fields');
-      return;
-    }
-    let data = {'date_from': this.datePipe.transform(this.date_from, 'dd/MM/yyyy'),
-                'date_to': this.datePipe.transform(this.date_to, 'dd/MM/yyyy')};
+    exportClick(report_form) {
+        if (report_form.invalid) {
+            this.notificationServiceBus.launchNotification(true, 'Please select all fields');
+            return;
+        }
 
-    this.spinnerService.requestInProcess(true);
-    this.bookingService.exportBookings(data)
-        .subscribe((res: any) => {
+        let data = {
+            'date_from': this.datePipe.transform(this.date_from, 'dd/MM/yyyy'),
+            'date_to': this.datePipe.transform(this.date_to, 'dd/MM/yyyy'),
+            'export_type': this.exportType
+        };
+
+        this.spinnerService.requestInProcess(true);
+        this.bookingService.exportBookings(data)
+            .subscribe((res: any) => {
                 if (res.status === 204) {
                     this.notificationServiceBus.launchNotification(false, `Export is successful. Please check your email.`);
                 }
                 this.spinnerService.requestInProcess(false);
             },
-            err => {
-                this.spinnerService.requestInProcess(false);
-                let e = err.json() || 'There is some error on server side';
-                this.notificationServiceBus.launchNotification(true, e);
-            });
-  }
+                err => {
+                    this.spinnerService.requestInProcess(false);
+                    let e = err.json() || 'There is some error on server side';
+                    this.notificationServiceBus.launchNotification(true, e);
+                });
+    }
 
-  private isBusinessVicdeaf(): boolean {
-    return (GLOBAL.currentUser.business_name === 'Vicdeaf');
-  }
+    private isBusinessVicdeaf(): boolean {
+        return (GLOBAL.currentUser.business_name === 'Vicdeaf');
+    }
 }
