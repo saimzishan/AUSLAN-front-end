9eba773fd74b6c7b317b9e93a9c4a04dc8be3a3d
M_Bilal <mbilal5445@gmail.com>
2018-04-02 15:31:54 +0500

    change fun name and fix PR comments

--- src/app/booking-management/booking-detail/booking-detail.component.ts
+++ src/app/booking-management/booking-detail/booking-detail.component.ts
@@ -169,7 +169,7 @@ export class BookingDetailComponent implements OnInit, OnDestroy {
                 this.oldInterpreterPreference = jsonData.preference_allocations_attributes;
                 this.bookingModel.documents_attributes = [];
                 this.bookingDate = new Date(this.datePipe.transform(this.bookingModel.venue.start_time_iso, 'MM/dd/yyyy'));
-                this.isRecurrenceDayCheckboxDisabled();
+                this.recurrenceDayCheckboxChecked();
                 this.setTime();
                 this.natureOfApptChange(null);
                 this.checkInterpreterBoxes();
@@ -821,7 +821,7 @@ export class BookingDetailComponent implements OnInit, OnDestroy {
                 || endDate.getSeconds() > 0)) || endDate.getHours() > 20);
     }
 
-    public isRecurrenceDayCheckboxDisabled() {
+    public recurrenceDayCheckboxChecked() {
         this.repeat_days.forEach(day => {
             if (this.bookingDate && this.bookingDate.getDay() === this.repeat_days.indexOf(day)) {
                 this.repeat_days[this.repeat_days.indexOf(day)].selected = true;
