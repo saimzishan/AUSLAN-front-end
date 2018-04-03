b5e22ae47de450dde3b668b544589fa9a8d82d40
NomiJ <nauman.qazi@systematicbytes.com>
2018-04-03 14:25:29 +1000

    booking job selected to message route

--- src/app/messaging/inbox/inbox.component.ts
+++ src/app/messaging/inbox/inbox.component.ts
@@ -27,7 +27,7 @@ export class InboxComponent implements OnInit, OnDestroy {
   message_body;
   message_tage = '-000000';
   checked = false;
-  isTagShow = true;
+  isTagShow = false;
   messages;
   selected = -1;
   loginUserID = GLOBAL.currentUser.id;
