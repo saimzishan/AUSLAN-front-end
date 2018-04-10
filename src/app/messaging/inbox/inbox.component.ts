580228eebd2378194c341294a70e85fe661e4254
NomiJ <nauman.qazi@systematicbytes.com>
2018-04-10 13:52:10 +1000

    fixed html struct for messages

--- src/app/messaging/inbox/inbox.component.ts
+++ src/app/messaging/inbox/inbox.component.ts
@@ -39,16 +39,20 @@ export class InboxComponent implements OnInit, AfterViewChecked, OnDestroy {
     totalItems = 0;
     selectedMessageThread = 0;
     messageCount = -1;
+    searchInterpreterQuery = ''
     public config: PerfectScrollbarConfigInterface = {};
 
     @ViewChild(PerfectScrollbarComponent) componentScroll: PerfectScrollbarComponent;
+
     constructor(private userService: UserService, private notificationServiceBus: NotificationServiceBus, public platformLocation: PlatformLocation,
                 private messagingService: MessagingService, private _location: Location, public spinnerService: SpinnerService,
                 private rolePermission: RolePermission, private router: Router, private route: ActivatedRoute) {
     }
+
     ngAfterViewChecked() {
 
     }
+
     ngOnInit() {
         this.business_id = GLOBAL.currentUser.business_id;
         this.sub = this.route.params.subscribe(params => {
@@ -66,6 +70,7 @@ export class InboxComponent implements OnInit, AfterViewChecked, OnDestroy {
             }
         });
     }
+
     loadMore() {
         this.messagePage += 1;
         if (this.isCurrentUserAdminOrBookingOfficer()) {
@@ -96,20 +101,21 @@ export class InboxComponent implements OnInit, AfterViewChecked, OnDestroy {
                     this.notificationServiceBus.launchNotification(true, e);
                 });
     }
+
     getInterpreterMessage(id) {
         this.spinnerService.requestInProcess(true);
         this.messagingService.getInterpreterMessage(id, this.business_id, this.messagePage)
             .subscribe((res: any) => {
-                if (res.status === 200) {
-                    this.messageCount = res.data.messages_count;
-                    this.messages = res.data.messages.reverse();
-                    setTimeout(() => {
-                        this.componentScroll.directiveRef.scrollToBottom();
-                        this.spinnerService.requestInProcess(false);
-                    }, 500);
+                    if (res.status === 200) {
+                        this.messageCount = res.data.messages_count;
+                        this.messages = res.data.messages.reverse();
+                        setTimeout(() => {
+                            this.componentScroll.directiveRef.scrollToBottom();
+                            this.spinnerService.requestInProcess(false);
+                        }, 500);
 
-                }
-            },
+                    }
+                },
                 errors => {
                     this.spinnerService.requestInProcess(false);
                     let e = errors.json();
@@ -147,7 +153,7 @@ export class InboxComponent implements OnInit, AfterViewChecked, OnDestroy {
         let message_tag = this.isTagShow && parseInt(this.message_tag, 10) > 0 ?
             this.message_tag : '';
         this.messagingService.sendMessages(this.loginUserID, url, this.message_body,
-            this.userId, message_tag )
+            this.userId, message_tag)
             .subscribe((res: any) => {
                 if (res.status === 200) {
                     this.notificationServiceBus.launchNotification(false, 'Message sent successfully..');
@@ -183,17 +189,20 @@ export class InboxComponent implements OnInit, AfterViewChecked, OnDestroy {
     hasTag(message) {
         return Boolean(message.tag) && message.tag.length > 1;
     }
+
     onTagClicked(message_tag) {
         let route = GLOBAL.currentUser instanceof Interpreter || GLOBAL.currentUser instanceof OrganisationalRepresentative
-            || GLOBAL.currentUser instanceof IndividualClient
-                ? 'job-detail' : 'booking-job';
+        || GLOBAL.currentUser instanceof IndividualClient
+            ? 'job-detail' : 'booking-job';
         this.router.navigate(['/booking-management/' + message_tag, route]);
     }
+
     getMessage(message) {
         let ind = message.indexOf('#');
         let messageTag = message.substring(ind + 1, message.length);
         return messageTag;
     }
+
     isCurrentUserAdminOrBookingOfficer(): boolean {
         return Boolean(GLOBAL.currentUser instanceof Administrator || GLOBAL.currentUser instanceof BookingOfficer);
     }
@@ -212,9 +221,19 @@ export class InboxComponent implements OnInit, AfterViewChecked, OnDestroy {
         let lastMesgDay = lastMesgDate.substring(8, 10);
         return (+lastMesgDay === curentDay);
     }
+
     getPage(page: number) {
         this.messageThreadPage = page;
         this.getAllMessageThreads(this.business_id);
+    }
+
+    searchInterpreter() {
+
+    }
+
+    clear() {
+        this.searchInterpreterQuery = '';
+        this.searchInterpreter();
     }
 
 }
