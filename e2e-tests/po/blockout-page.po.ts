a009ed75726d4a05b78db71c22e9741c87ef8a8c
saimzishan <saimzishan@gmail.com>
2018-04-11 12:44:28 +0500

    checks on date, casing problem

--- e2e-tests/po/blockout-page.po.ts
+++ e2e-tests/po/blockout-page.po.ts
@@ -72,6 +72,81 @@ export class BlockoutPagePo extends PageObject {
             expect(val).to.be.eq('09:00 PM');
         });
     }
+    chengeStartTimeWithOutYear = () => {
+        let startTime = this.getElementByCss('input[name="dpEventDate_st"]');
+        let today = new Date();
+        today.setDate(today.getDate() + 7);
+        const currentDate = [
+            Heroku.prettyDate(today.getDate()),
+            Heroku.prettyDate(today.getMonth() + 1)
+        ].join('/');
+        startTime.clear();
+        startTime.sendKeys(currentDate + ' 08:00 PM');
+        this.clickOutSide();
+        return startTime.getAttribute('value').then((val) => {
+            expect(val).to.be.eq('');
+        });
+    }
+    chengeStartTimeWithWrongYear = () => {
+        let startTime = this.getElementByCss('input[name="dpEventDate_st"]');
+        let today = new Date();
+        today.setDate(today.getDate() + 7);
+        const currentDate = [
+            Heroku.prettyDate(today.getDate()),
+            Heroku.prettyDate(today.getMonth() + 1),
+            today.getFullYear().toString().substring(0, 3)
+        ].join('/');
+        startTime.clear();
+        startTime.sendKeys(currentDate + ' 08:00 PM');
+        this.clickOutSide();
+        return startTime.getAttribute('value').then((val) => {
+            expect(val).to.be.eq('');
+        });
+    }
+    chengeStartTimeWithWrongInput = () => {
+        let startTime = this.getElementByCss('input[name="dpEventDate_st"]');
+        let today = new Date();
+        today.setDate(today.getDate() + 7);
+        const currentDate = 'abcdefgh';
+        startTime.clear();
+        startTime.sendKeys(currentDate + ' 08:00 PM');
+        this.clickOutSide();
+        return startTime.getAttribute('value').then((val) => {
+            expect(val).to.be.eq('');
+        });
+    }
+    chengeStartTimeWithWrongMonth = () => {
+        let startTime = this.getElementByCss('input[name="dpEventDate_st"]');
+        let today = new Date();
+        today.setDate(today.getDate() + 7);
+        const currentDate = [
+            Heroku.prettyDate(today.getDate()),
+            Heroku.prettyDate(today.getMonth() + 12),
+            today.getFullYear().toString()
+        ].join('/');
+        startTime.clear();
+        startTime.sendKeys(currentDate + ' 08:00 PM');
+        this.clickOutSide();
+        return startTime.getAttribute('value').then((val) => {
+            expect(val).to.be.eq('');
+        });
+    }
+    chengeStartTimeWithWrongDay = () => {
+        let startTime = this.getElementByCss('input[name="dpEventDate_st"]');
+        let today = new Date();
+        today.setDate(today.getDate() + 7);
+        const currentDate = [
+            Heroku.prettyDate(today.getDate() + 31),
+            Heroku.prettyDate(today.getMonth() + 1),
+            today.getFullYear().toString()
+        ].join('/');
+        startTime.clear();
+        startTime.sendKeys(currentDate + ' 08:00 PM');
+        this.clickOutSide();
+        return startTime.getAttribute('value').then((val) => {
+            expect(val).to.be.eq('');
+        });
+    }
     changeEndTimeOFBlockout = () => {
         let endTime = this.getElementByCss('input[name="dpEventDate_endtime"]');
         endTime.clear();
